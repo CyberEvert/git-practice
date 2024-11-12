edab0ea9db1f24c6bc46d44cd44fd4d30d356f06
CyberEvert <evert.edesi@gmail.com>
2024-11-12 11:30:28 +0200

    New text with scanner

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello people!");
         String text = "New car";
         System.out.println(text);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("What is your age?");
 
         int number = 9;
         System.out.println(number);
